6d3d0c811896039ba036d93d54f7c31a7aad0eb9
Edward.yang <edwardhey@gmail.com>
2012-12-16 16:57:23 +0800

    jj

--- git_commit.sh
+++ git_commit.sh
@@ -3,5 +3,5 @@ if [ $# -lt 1 ]; then
 	echo "usage:$0 [comment]"
 	exit 0
 fi
-git add CREDITS EXPERIMENTAL config.m4 config.w32 php_ttmp.h tests ttmp.c ttmp.php git_commit.sh
+git add CREDITS EXPERIMENTAL config.m4 config.w32 php_ttmp.h tests ttmp.c ttmp.php git_commit.sh README.md .gitignore
 git commit -m $1
